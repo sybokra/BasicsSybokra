36add0091569bd09d7cd720bf4fffe5aa879a68c
curtin <curtinliu@163.com>
2019-04-13 23:41:37 +0800

    创建项目

---
+++ src/main/java/com/sybokra/basics/SybokraApplication.java
@@ -0,0 +1,13 @@
+package com.sybokra.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SybokraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SybokraApplication.class, args);
+	}
+
+}
